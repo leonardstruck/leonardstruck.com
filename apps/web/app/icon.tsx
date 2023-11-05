27a175de6e17e75f86efdc44ccc44bcce4c06df5
Leonard Struck <me@leonardstruck.com>
2023-11-05 09:08:19 +0100

    add custom favicon

---
+++ apps/web/app/icon.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/no-unknown-property -- tailwind */
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+    width: 32,
+    height: 32,
+}
+export const contentType = 'image/png'
+
+export const runtime = "edge"
+
+// Image generation
+export default function Icon(): ImageResponse {
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    height: '100%',
+                    width: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    fontSize: '1.3rem',
+                }}
+                tw="text-xl font-black text-neutral-50 tracking-wide bg-neutral-900 rounded"
+            >
+                LS
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported icons size metadata
+            // config to also set the ImageResponse's width and height.
+            ...size,
+        }
+    )
+}
