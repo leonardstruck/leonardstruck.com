189f0b1608476cb2bd6fc9edd9bb45098b9d515a
Leonard Struck <me@leonardstruck.com>
2023-11-08 14:03:52 +0100

    Refactor TwoColLayout component to use updated RichTextRenderer

--- apps/web/renderers/block/blocks/two-col-layout/index.tsx
+++ apps/web/renderers/block/blocks/two-col-layout/index.tsx
@@ -1,18 +1,32 @@
 import type { ITwoColLayoutBlock } from "cms/src/payload-types";
-import RichTextRenderer from "@/renderers/rich-text/rich-text-renderer";
-import type { RootNode } from "@/renderers/rich-text/nodes/root";
+import RichTextRenderer from "@/renderers/rich-text";
+import type { BaseNode } from "@/renderers/rich-text/types";
+import type { BlockInterface } from "../../types";
 
-interface TwoColLayoutProps {
-    block: ITwoColLayoutBlock
-}
-
-export default function TwoColLayout({ block }: TwoColLayoutProps): React.ReactNode {
-    const nodes = (block as { leftCol?: { root?: RootNode }, rightCol?: { root?: RootNode } });
+function Component(block: ITwoColLayoutBlock): React.ReactNode {
+    const nodes = (block as { leftCol?: { root: BaseNode }, rightCol?: { root: BaseNode } });
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-10 gap-4">
-            <div className="md:col-span-6">{nodes.leftCol?.root ? <RichTextRenderer node={nodes.leftCol.root} /> : null}</div>
-            <div className="md:col-span-4">{nodes.rightCol?.root ? <RichTextRenderer node={nodes.rightCol.root} /> : null}</div>
+            <div className="md:col-span-6">{nodes.leftCol?.root ? RichTextRenderer.renderNode("root", nodes.leftCol.root) : null}</div>
+            <div className="md:col-span-4">{nodes.rightCol?.root ? RichTextRenderer.renderNode("root", nodes.rightCol.root) : null}</div>
         </div>
     )
 }
+
+const TwoColLayoutBlock: BlockInterface<ITwoColLayoutBlock> = {
+    render: (block, key) => <Component {...block} key={key} />,
+    prefetch: async (block, queryClient) => {
+        const nodes = (block as { leftCol?: { root: BaseNode }, rightCol?: { root: BaseNode } });
+
+        if (nodes.leftCol?.root) {
+            await RichTextRenderer.prefetchNode("root", nodes.leftCol.root, queryClient);
+        }
+
+        if (nodes.rightCol?.root) {
+            await RichTextRenderer.prefetchNode("root", nodes.rightCol.root, queryClient);
+        }
+    }
+}
+
+export default TwoColLayoutBlock;
