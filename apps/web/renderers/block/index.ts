c83acd43eef4613fd8ffa74054a140d45b5448fb
Leonard Struck <me@leonardstruck.com>
2023-11-14 13:16:25 +0100

    Add TwoColLayoutBlock to block renderer factory

--- apps/web/renderers/block/index.ts
+++ apps/web/renderers/block/index.ts
@@ -1,9 +1,11 @@
 import HeroWithImageBlock from "./blocks/hero-with-image";
+import TwoColLayoutBlock from "./blocks/two-col-layout";
 import { BlockRendererFactory } from "./factory";
 
 const Renderer = new BlockRendererFactory();
 
 // Register blocks below
 Renderer.registerBlock("hero-with-image", HeroWithImageBlock)
+Renderer.registerBlock("two-col-layout", TwoColLayoutBlock)
 
 export default Renderer;
