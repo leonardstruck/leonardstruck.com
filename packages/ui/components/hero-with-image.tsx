2f29f28663497c4d6d18e17c8f0a50c7ad6c7248
Leonard Struck <me@leonardstruck.com>
2023-11-14 13:11:19 +0100

    fix alignment

--- packages/ui/components/hero-with-image.tsx
+++ packages/ui/components/hero-with-image.tsx
@@ -1,3 +1,5 @@
+import { cn } from "../cn";
+
 interface HeroWithImageProps {
     title: string;
     imageSrc: string;
@@ -8,9 +10,17 @@ interface HeroWithImageProps {
 export function HeroWithImage({ title, imageSrc, imageAlt, asImage }: HeroWithImageProps): React.ReactNode {
     const Image = asImage || "img";
     return (
-        <div className="grid grid-cols-6 items-center">
-            <h1 className="font-mono [text-wrap:balance] translate-y-3/4 sm:translate-y-1/3 lg:translate-y-0 text-5xl sm:text-6xl md:text-8xl lg:text-9xl font-black mix-blend-difference z-10 row-span-full col-span-4 col-start-1">{title}</h1>
-            <Image alt={imageAlt} className="rounded-4xl col-start-2 md:col-start-3 lg:col-start-4 col-end-7 row-span-full" src={imageSrc} />
+        <div className="grid grid-cols-6 grid-rows-4 items-center">
+            <Image alt={imageAlt} className={cn(
+                "col-span-5 col-start-2 row-span-4 row-start-1",
+                "lg:col-span-4 lg:col-start-3",
+                "rounded-xl"
+            )} src={imageSrc} />
+            <h1 className={cn(
+                "font-mono font-black mix-blend-exclusion",
+                "text-6xl md:text-8xl lg:text-9xl",
+                "col-span-full row-span-2 row-start-3"
+            )}>{title}</h1>
         </div >
     )
 };
